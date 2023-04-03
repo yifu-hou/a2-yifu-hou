1d1086321c27a67bc4660a36ba0a3e2305d0fa8f
yifu-hou <yifu@uchicago.edu>
2023-04-03 14:13:10 -0500

    set up q1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # a2-yifu-hou
 repository for assignment 2
+
+### Q1
+
+Here's an `sbatch` script that will compile simulation function from Assignment 1 Question (1b) using ahead of time (`AOT`). Only a single core is requested in this sbatch script.
+
+### Q2
+
+Parallelizing simulation via `mpi4py` multi-processing and the parallelization strategy from Assignment 1, Question (2).
---
+++ q1.py
@@ -0,0 +1,33 @@
+from numba.pycc import CC
+
+S = 1000
+T = 4160
+rho, mu, sigma = 0.5, 3.0, 1.0
+z_0 = mu
+
+cc = CC('test_aot')
+
+@cc.export('simulate_aot', 'f8[:,:](f8[:,:], f4, f4)')
+def simulate_aot(z_mat, rho, mu):
+    '''
+    Compute S simulations of a lifetime T - AOT ACCELERATED
+    Inputs:
+        z_mat: (numpy) container for simulation results
+        rho: (float) degree of persistence
+        mu: long-run average
+    Return:
+        z_mat: (numpy) simulation result of size (T, S)
+    '''
+
+    T, S = z_mat.shape
+    for s_ind in range(S):
+        z_tm1 = z_0
+        for t_ind in range(T):
+            e_t = eps_mat[t_ind, s_ind]
+            z_t = rho * z_tm1 + (1 - rho) * mu + e_t
+            z_mat[t_ind, s_ind] = z_t
+            z_tm1 = z_t
+
+    return z_mat
+
+cc.compile()
